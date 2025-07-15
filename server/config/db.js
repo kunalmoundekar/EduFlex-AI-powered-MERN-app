71b22809ae36383b0ef76e8ccd416ceb67929025
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-16 01:36:34 +0530

    Removed unused React boilerplate files and updated server config

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${"👍"}`);
   } catch (err) {
     console.error("MongoDB Error:", err.message);
     process.exit(1);
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,8 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send("EduFlex API Running"));
 
