22b39095a4a8d1896c5bbf1b03310f3c7240e6be
kunalmoundekar <kunalmoundekar94@gmail.com>
2025-07-16 00:27:40 +0530

    for createing a backend

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// server/server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("EduFlex API Running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
